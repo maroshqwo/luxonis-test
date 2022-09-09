071fde2c87f275be15862046b4abe32862267881
Maroš Kyjovský <mmmaroshhh@gmail.com>
2022-09-09 17:21:48 +0200

    feat:(made pagination working better)

--- luxonis-test-fe/src/components/Pagination.tsx
+++ luxonis-test-fe/src/components/Pagination.tsx
@@ -19,21 +19,37 @@ export const Pagination: FC<PaginationProps> = (props) => {
   }, []);
 
   useEffect(() => {
-    const lastPage = Math.ceil(propertiesCount / 24);
-    const currentPage = parseInt(location.pathname.split("/")[1]) || 1;
-    var pages = [];
-    if (currentPage === 1 || currentPage === 2) {
-      pages = [1, 2, 3, 0, lastPage];
-    } else if (currentPage === lastPage || currentPage === lastPage - 1) {
-      pages = [1, 0, lastPage - 2, lastPage - 1, lastPage];
-    } else if (currentPage === 3) {
-      pages = [1, 2, 3, 4, 0, lastPage];
-    } else if (currentPage === lastPage - 2) {
-      pages = [1, 0, lastPage - 3, lastPage - 2, lastPage - 1, lastPage];
-    } else {
-      pages = [1, 0, currentPage - 1, currentPage, currentPage + 1, 0, lastPage];
+    if (!propertiesCount) return;
+    const fp = 1
+    const lp = Math.ceil(propertiesCount / 24);
+    const cp = parseInt(location.pathname.split("/")[1]) || 1;
+    const pages = [];
+    if (lp >= 5) {
+    for(let i = fp; i <= lp; i++) {
+      if (i === cp || i === cp - 1 || i === cp + 1 || i === fp || i === lp) {
+        pages.push(i);
+      }
+      if (cp === 1 && i === 2) {
+        pages.push(3);
+        pages.push(4);
+      }
+      if (cp === 2 && i === 3) {
+        pages.push(4);
+      }
+      if (cp === lp - 1 && i === lp - 3) {
+        pages.push(lp - 3);
+      }
+      if (cp === lp && i === lp - 2) {
+        pages.push(lp - 3);
+        pages.push(lp - 2);
+      }
     }
-    console.log(pages);
+  } else {
+    for(let i = fp; i <= lp; i++) {
+      pages.push(i);
+    }
+  }
+    
     setPagination(pages);
 
   }, [location, propertiesCount]);
@@ -45,7 +61,7 @@ export const Pagination: FC<PaginationProps> = (props) => {
       <HStack marginInlineStart={"2px"}>
         {pagination?.map((page) => (
           page =! NaN &&
-          page != 0 ? (
+          (
             <Button
             key={page}
             variant="ghost"
@@ -61,10 +77,6 @@ export const Pagination: FC<PaginationProps> = (props) => {
           >
             {page}
           </Button>
-          ) : (
-            <Text key={Math.ceil(Math.random()*10000)} fontSize="sm" color="gray.500" marginInlineStart={"2px"} marginInlineEnd={"2px"}>
-              ...
-            </Text>
           )
           ))}
       </HStack>
